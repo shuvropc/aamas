3ad12df9657c5e35a15b4771332ad11ccb698506
shuvropc <37395674+shuvropc@users.noreply.github.com>
2018-03-17 16:44:31 +0600

    sloved

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-   function index(){
+   function index()
+   {
        return view('index');
-       return view('index2');
-       //asjfksjfhkdahvadklvhbakbhvaba
-       //jafsdkbgkabgakj
    }
-
 }
