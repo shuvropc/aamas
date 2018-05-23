9cf86dc104775ce8db57fa0bcb51ed50f407b2d5
ASIF TANIM <tanimasif23@gmail.com>
2018-05-23 07:55:32 +0600

    product model

--- app/Product.php
+++ app/Product.php
@@ -3,10 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use App\Category;
 use App\Vendor;
 
 class Product extends Model
 {
     //
+    public function Products(){
+        $sql = "SELECT * FROM `products` 
+                INNER JOIN categories ON products.category_id = categories.id 
+                INNER JOIN vendors ON products.vendor_id = vendors.id
+                INNER JOIN details ON products.details_id = details.id";
+
+
+        return DB::select($sql);
+    }
+
 }
