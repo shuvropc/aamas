ee5ce3244ff6d813ddde44344029aa63b2d062a1
Mahadi Hasan <mahadih384@gmail.com>
2018-05-31 03:42:13 +0600

    contact page and some modification

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.user-layout-without-sidebar')
 
 
-@section('title', 'Wishlist')
+@section('title', 'Contact')
 
 
 @section('content')
@@ -21,49 +21,51 @@
     <div class="container">
         <div class="contact-page">
             <div class="row">
-
                 <div class="col-md-12 contact-map outer-bottom-vs">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.0080692193424!2d80.29172299999996!3d13.098675000000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a526f446a1c3187%3A0x298011b0b0d14d47!2sTransvelo!5e0!3m2!1sen!2sin!4v1412844527190" width="600" height="450" style="border:0"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.903129616355!2d90.42537451452581!3d23.82204348455418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c648a7768e31%3A0xebb579b6a6bc3c67!2sAmerican+International+University-Bangladesh+(AIUB)!5e0!3m2!1sen!2sbd!4v1527681146614" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
                 <div class="col-md-9 contact-form">
+
                     <div class="col-md-12 contact-title">
                         <h4>Contact Form</h4>
                     </div>
-                    <div class="col-md-4 ">
-                        <form class="register-form" role="form">
+                    <form method="post" class="register-form" role="form">
+                        @csrf
+                        <div class="col-md-4 ">
                             <div class="form-group">
-                                <label class="info-title" for="exampleInputName">Your Name <span>*</span></label>
-                                <input type="email" class="form-control unicase-form-control text-input" id="exampleInputName" placeholder="">
+                                <label class="info-title" for="exampleInputName">Your Name <span>* </span></label>
+                                <input type="text" name="name" class="form-control unicase-form-control text-input" id="exampleInputName" placeholder="">
+                                <span class="text-danger">{{$errors->has('name') ? $errors->first('name'): ''}}</span>
                             </div>
-                        </form>
-                    </div>
-                    <div class="col-md-4">
-                        <form class="register-form" role="form">
+                        </div>
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
-                                <input type="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" placeholder="">
+                                <input type="email" name="email" class="form-control unicase-form-control text-input" id="exampleInputEmail1" placeholder="">
+                                <span class="text-danger">{{$errors->has('email') ? $errors->first('email'): ''}}</span>
+
                             </div>
-                        </form>
-                    </div>
-                    <div class="col-md-4">
-                        <form class="register-form" role="form">
+                        </div>
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label class="info-title" for="exampleInputTitle">Title <span>*</span></label>
-                                <input type="email" class="form-control unicase-form-control text-input" id="exampleInputTitle" placeholder="Title">
+                                <input type="text" name="title" class="form-control unicase-form-control text-input" id="exampleInputTitle" placeholder="Title">
+                                <span class="text-danger">{{$errors->has('title') ? $errors->first('title'): ''}}</span>
+
                             </div>
-                        </form>
-                    </div>
-                    <div class="col-md-12">
-                        <form class="register-form" role="form">
+                        </div>
+                        <div class="col-md-12">
                             <div class="form-group">
                                 <label class="info-title" for="exampleInputComments">Your Comments <span>*</span></label>
-                                <textarea class="form-control unicase-form-control" id="exampleInputComments"></textarea>
+                                <textarea class="form-control unicase-form-control" name="comment" id="exampleInputComments"></textarea>
+                                <span class="text-danger">{{$errors->has('comment') ? $errors->first('comment'): ''}}</span>
+
                             </div>
-                        </form>
-                    </div>
-                    <div class="col-md-12 outer-bottom-small m-t-20">
-                        <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Send Message</button>
-                    </div>
+                        </div>
+                        <div class="col-md-12 outer-bottom-small m-t-20">
+                            <input type="submit" value="Send Message" class="btn-upper btn btn-primary checkout-page-button ">
+                        </div>
+                    </form>
                 </div>
                 <div class="col-md-3 contact-info">
                     <div class="contact-title">
